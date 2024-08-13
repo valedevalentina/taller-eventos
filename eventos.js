f6c09a41dd05028427739eba24906b5be8013b18
Valentina <valecunha7@gmail.com>
2024-08-13 16:43:56 -0300

    Solución problema de propagación del evento click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,8 @@
-function saludar() {
+function saludar(event) {
+    event.stopPropagation();  // Evita que el evento se propague al div
     alert('¡Hola!');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('div').addEventListener('click', function() {
-        alert('Hola! Soy el div');
-    });
+document.querySelector('div').addEventListener('click', function() {
+    alert('Hola! Soy el div');
 });
